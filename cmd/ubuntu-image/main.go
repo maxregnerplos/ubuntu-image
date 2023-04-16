269875ddd533598645a9b6ee47df73d408ce71db
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2023-04-16 10:08:38 +0200

    Update main.go

--- cmd/ubuntu-image/main.go
+++ cmd/ubuntu-image/main.go
@@ -1,3 +1,5 @@
+package main
+
 import (
 	"fmt"
 	"os"
@@ -21,148 +23,57 @@ var (
 	size       int
 )
 
-// Version holds the ubuntu-image version number
-// this is usually overridden at build time
-var Version string = ""
-
-// helper variables for unit testing
-var osExit = os.Exit
-var captureStd = helper.CaptureStd
-var stateMachineInterface statemachine.SmInterface
-var imageType string = ""
-
-var stateMachineLongDesc = `Options for controlling the internal state machine.
-Other than -w, these options are mutually exclusive. When -u or -t is given,
-the state machine can be resumed later with -r, but -w must be given in that
-case since the state is saved in a ubuntu-image.gob file in the working directory.`
-
-func executeStateMachine(commonOpts *commands.CommonOpts, stateMachineOpts *commands.StateMachineOpts, ubuntuImageCommand *commands.UbuntuImageCommand) {
-	// Set up the state machine
-	if imageType == "snap" {
-		stateMachine := new(statemachine.SnapStateMachine)
-		stateMachine.Opts = ubuntuImageCommand.Snap.SnapOptsPassed
-		stateMachine.Args = ubuntuImageCommand.Snap.SnapArgsPassed
-		stateMachine.SetCommonOpts(commonOpts, stateMachineOpts)
-		stateMachineInterface = stateMachine
-	} else if imageType == "classic" {
-		stateMachine := new(statemachine.ClassicStateMachine)
-		stateMachine.Opts = ubuntuImageCommand.Classic.ClassicOptsPassed
-		stateMachine.Args = ubuntuImageCommand.Classic.ClassicArgsPassed
-		stateMachine.SetCommonOpts(commonOpts, stateMachineOpts)
-		stateMachineInterface = stateMachine
-	}
-
-	// set up, run, and tear down the state machine
-	if err := stateMachineInterface.Setup(); err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-		osExit(1)
-		return
-	}
-
-	if err := stateMachineInterface.Run(); err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-		osExit(1)
-		return
-	}
-
-	if err := stateMachineInterface.Teardown(); err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-		osExit(1)
-		return
-	}
-
+func init() {
+	cmd.Flags().StringVarP(&outputFile, "output", "o", "mros.img", "output image path")
+	cmd.Flags().StringVarP(&inputDir, "input", "i", ".", "input directory path")
+	cmd.Flags().IntVar(&size, "size", 2048, "size of the image in megabytes")
 }
 
 func main() {
-	// instantiate structs for
-	commonOpts := new(commands.CommonOpts)
-	stateMachineOpts := new(commands.StateMachineOpts)
-	ubuntuImageCommand := new(commands.UbuntuImageCommand)
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
 
-	// set up the go-flags parser for command line options
-	parser := flags.NewParser(ubuntuImageCommand, flags.Default)
-	parser.AddGroup("State Machine Options", stateMachineLongDesc, stateMachineOpts)
-	parser.AddGroup("Common Options", "Options common to both commands", commonOpts)
-
-	// go-flags can be overzealous about printing errors that aren't actually errors
-	// so we capture stdout/stderr while parsing and later decide whether to print
-	stdout, restoreStdout, err := captureStd(&os.Stdout)
+func runCreate(cmd *cobra.Command, args []string) error {
+	inputDir, err := filepath.Abs(inputDir)
 	if err != nil {
-		fmt.Printf("Failed to capture stdout: %s\n", err.Error())
-		osExit(1)
-		return
+		return err
 	}
-	defer restoreStdout()
 
-	stderr, restoreStderr, err := captureStd(&os.Stderr)
+	outputFile, err := filepath.Abs(outputFile)
 	if err != nil {
-		fmt.Printf("Failed to capture stderr: %s\n", err.Error())
-		osExit(1)
-		return
-	}
-	defer restoreStderr()
-
-	// Parse the options provided and handle specific errors
-	if _, err := parser.Parse(); err != nil {
-		if e, ok := err.(*flags.Error); ok {
-			switch e.Type {
-			case flags.ErrHelp:
-				restoreStdout()
-				restoreStderr()
-				readStdout, err := io.ReadAll(stdout)
-				if err != nil {
-					fmt.Printf("Error reading from stdout: %s\n", err.Error())
-					osExit(1)
-					return
-				}
-				fmt.Println(string(readStdout))
-				osExit(0)
-				return
-			case flags.ErrCommandRequired:
-				// if --resume was given, this is not an error
-				if !stateMachineOpts.Resume && !commonOpts.Version {
-					restoreStdout()
-					restoreStderr()
-					readStderr, err := io.ReadAll(stderr)
-					if err != nil {
-						fmt.Printf("Error reading from stderr: %s\n", err.Error())
-						osExit(1)
-						return
-					}
-					fmt.Printf("Error: %s\n", string(readStderr))
-					osExit(1)
-					return
-				}
-				break
-			default:
-				restoreStdout()
-				restoreStderr()
-				fmt.Printf("Error: %s\n", err.Error())
-				osExit(1)
-				return
-			}
-		}
+		return err
 	}
 
-	// restore stdout
-	restoreStdout()
-	restoreStderr()
+	fmt.Printf("Creating mros image from %s, size %d MB\n", inputDir, size)
 
-	// in case user only requested version number, print and exit
-	if commonOpts.Version {
-		// we expect Version to be supplied at build time or fetched from the snap environment
-		if Version == "" {
-			Version = os.Getenv("SNAP_VERSION")
-		}
-		fmt.Printf("ubuntu-image %s\n", Version)
-		osExit(0)
-		return
+	config := &types.Config{
+		TargetSizeMB: size,
+
+		// Add mros-specific configuration options here
+		// (e.g. kernel version, partition layout, etc.)
 	}
 
-	if parser.Command.Active != nil && imageType == "" {
-		imageType = parser.Command.Active.Name
+	builder, err := image.NewBuilder(config)
+	if err != nil {
+		return err
 	}
 
-	// let the state machine handle the image build
-	executeStateMachine(commonOpts, stateMachineOpts, ubuntuImageCommand)
+	// Add mros-specific partitions here
+	parts := []partition.Partition{
+		partition.NewPartition("boot", partition.PartitionTypeEFI, 0, "512M"),
+		partition.NewPartition("root", partition.PartitionTypeLinux, 1, ""),
+	}
+
+	builder.SetPartitions(parts)
+
+	if err := builder.Build(inputDir, outputFile); err != nil {
+		return err
+	}
+
+	fmt.Printf("Created mros image at %s\n", outputFile)
+
+	return nil
 }
